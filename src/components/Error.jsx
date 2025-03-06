9f553888a86345f709501d9c3f60be5e3170a509
Michele Mandanici <michimandanici@tim.it>
2025-03-06 17:22:22 +0100

    .

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 import { Alert } from 'react-bootstrap'
 
 const Error = () => (
-  <Alert variant="danger">Errore nel caricamento del profilo</Alert>
+  <Alert variant="danger">Errore nel caricamento dei dati</Alert>
 )
 
 export default Error
