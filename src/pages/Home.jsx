602b1937fb36c5cfd3233994f45dc0b28376dd70
Michele Mandanici <michimandanici@tim.it>
2025-03-18 15:06:58 +0100

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,40 +5,12 @@ import Carousel from '../components/HomeCarousel'
 const Home = () => {
   return (
     <>
-      <Container className="position-relative bg-white">
+      <Container fluid className="py-5">
         <Row className="justify-content-center mt-3">
-          <Col xs={12} md={8} lg={6}>
+          <Col xs={12} md={8} lg={12}>
             <div className="d-flex justify-content-center">
               <Carousel />
             </div>
-            <h1>Benvenuto nel nostro sito</h1>
-            <p>
-              Questo è un esempio di testo che appare sotto l'immagine. Quando
-              scrolli, il contenuto si sovrappone all'immagine.
-            </p>
-            <p>Altro testo di esempio per riempire la pagina...</p>
-            <p>Altro testo di esempio per riempire la pagina...</p>
-            <p>
-              Altro testo di esempio per riempire la pagina...lorem ipsum lorem
-              ipsum lorem ipsumAltro testo di esempio per riempire la
-              pagina...lorem ipsum lorem ipsum lorem ipsumAltro testo di esempio
-              per riempire la pagina...lorem ipsum lorem ipsum lorem ipsumAltro
-              testo di esempio per riempire la pagina...lorem ipsum lorem ipsum
-              lorem ipsumAltro testo di esempio per riempire la pagina...lorem
-              ipsum lorem ipsum lorem ipsumAltro testo di esempio per riempire
-              la pagina...lorem ipsum lorem ipsum lorem ipsumAltro testo di
-              esempio per riempire la pagina...lorem ipsum lorem ipsum lorem
-              ipsumAltro testo di esempio per riempire la pagina...lorem ipsum
-              lorem ipsum lorem ipsumAltro testo di esempio per riempire la
-              pagina...lorem ipsum lorem ipsum lorem ipsumAltro testo di esempio
-              per riempire la pagina...lorem ipsum lorem ipsum lorem ipsumAltro
-              testo di esempio per riempire la pagina...lorem ipsum lorem ipsum
-              lorem ipsumAltro testo di esempio per riempire la pagina...lorem
-              ipsum lorem ipsum lorem ipsumAltro testo di esempio per riempire
-              la pagina...lorem ipsum lorem ipsum lorem ipsumAltro testo di
-              esempio per riempire la pagina...lorem ipsum lorem ipsum lorem
-              ipsum
-            </p>
           </Col>
         </Row>
       </Container>
