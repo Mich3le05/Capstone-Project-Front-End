3a10ad89c57d6d02ef59bb90544fcd90040012b6
Michele Mandanici <michimandanici@tim.it>
2025-03-22 19:31:59 +0100

    .

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -5,7 +5,7 @@ const Account = () => {
   return (
     <Container>
       <Row className="justify-content-center mt-3">
-        <Col xs={12} md={8} lg={6}>
+        <Col xs={12} md={8} lg={6} className="p-5">
           <h1>Acconut</h1>
           <LogoutButton />
         </Col>
